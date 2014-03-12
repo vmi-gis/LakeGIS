c4ff1e77ce50c2b664b74fc932b8397aecdae5f5
Konstantin Besedin <besedin.k@gmail.com>
2014-03-12 12:50:45 +0600

    Добавил приложение.

--- LakeGIS/LakeGIS/settings.py
+++ LakeGIS/LakeGIS/settings.py
@@ -57,8 +57,6 @@ WSGI_APPLICATION = 'LakeGIS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
