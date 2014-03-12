c91db47109ea0893c52fa6abd030d772ae9e2d0f
Konstantin Besedin <besedin.k@gmail.com>
2014-03-12 17:12:20 +0600

    Добавлены модели.
    Добавлены следующие модели:
    * Водоем
    * Лес
    * Населенный пункт
    * Шоссе
    * Железная дорога
    * База отдыха
    
    Все модели интегрированы в админку.

--- LakeGIS/lakegis_app/admin.py
+++ LakeGIS/lakegis_app/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from django.db.models import get_models, get_app
+from django.contrib.admin.sites import AlreadyRegistered
+import lakegis_app
 
 # Register your models here.
+app_models = get_app('lakegis_app')
+for model in get_models(app_models):
+    try:
+        admin.site.register(model, admin.OSMGeoAdmin)
+    except AlreadyRegistered:
+        pass
+
--- LakeGIS/lakegis_app/models.py
+++ LakeGIS/lakegis_app/models.py
@@ -1,3 +1,56 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class WaterModel(models.Model):
+    name = models.CharField(max_length = 50)
+    geom = models.MultiPolygonField()
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+class ForestModel(models.Model):
+    name = models.CharField(max_length = 50)
+    geom = models.MultiPolygonField()
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+class SettlementModel(models.Model):
+    name = models.CharField(max_length = 50)
+    geom = models.MultiPolygonField()
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+class RecreationCenterModel(models.Model):
+    name = models.CharField(max_length = 50)
+    geom = models.MultiPolygonField()
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+class HighwayModel(models.Model):
+    name = models.CharField(max_length = 50)
+    geom = models.MultiLineStringField()
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
+class RailwayModel(models.Model):
+    name = models.CharField(max_length = 50)
+    geom = models.MultiLineStringField()
+
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return self.name
+
