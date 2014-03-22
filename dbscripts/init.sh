c47dd06d66fb8bd32ba7bb9dc89159e54125c268
Konstantin Besedin <besedin.k@gmail.com>
2014-03-22 15:43:01 +0000

    Исправление опечаток.
    [skip ci]

--- dbscripts/init.sh
+++ dbscripts/init.sh
@@ -19,16 +19,15 @@ cd `dirname $0`
 
 echo "Script will create database \"$DB_NAME\" from template \"$TEMPLATE_DB_NAME\"."
 echo "Script will also create database user \"$DB_LOGIN\" with password $DB_LOGIN_PASSWORD. This user will be given full access to created database."
-echo "Will use \"$DB_SUPERUSER\" as database superuser. Depending on your PostgreSQL configuration, you may be asked to enter password several times"
-echo
-
+echo "Will use \"$DB_SUPERUSER\" as database superuser. Depending on your PostgreSQL configuration, you may be asked to enter password."
 echo
 
 psql -U $DB_SUPERUSER -d postgres -v "DB_NAME=$DB_NAME" -v"DB_LOGIN=$DB_LOGIN" -v "DB_LOGIN_PASSWORD=$DB_LOGIN_PASSWORD" -v "TEMPLATE_DB_NAME=$TEMPLATE_DB_NAME" -f init.sql 2>/dev/null
 
 if [ "$DB_NAME" != "$DEFAULT_DB_NAME" -o "$DB_LOGIN" != "$DEFAULT_DB_LOGIN" -o "$DB_LOGIN_PASSWORD" != "$DEFAULT_DB_LOGIN_PASSWORD" ]; then
+	echo
 	echo "+------------------------WARNING-------------------------+"
-	echo "|  Some settings changes you have made require custom    |"
+	echo "|  Some settings changes you have made, require custom   |"
        	echo "| django database configuration. Place following lines   |"
 	echo "| to your local_settings.py (LakeGIS knows how to handle |"
 	echo "| this file):                                            |"
